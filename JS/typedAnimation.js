8440a7434610289b8ba65b6a1f86544b3e9c3a03
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-06-19 21:25:06 +0500

    Creating portfoilio

--- JS/typedAnimation.js
+++ JS/typedAnimation.js
@@ -1,4 +1,4 @@
 let typedAnimation = new Typed('#element', {
-    strings: ["Game Developer", ", Web Developer", "&amp; Software Engineer."],
+    strings: ["A Game Developer", ", Web Developer", "&amp; Software Engineer."],
     typeSpeed: 75,
   });
