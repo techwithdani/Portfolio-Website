ac3468ff9d393e82ec3ba02860f8e6b71eb442f6
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-06-21 22:38:00 +0500

    Creating portfolio

--- JS/typedAnimation.js
+++ JS/typedAnimation.js
@@ -1,4 +1,4 @@
 let typedAnimation = new Typed('#element', {
-    strings: ["A Game Developer", ", Web Developer", "&amp; Software Engineer."],
+    strings: ["Game Developer", ", Web Developer", "&amp; Software Engineer."],
     typeSpeed: 75,
   });
